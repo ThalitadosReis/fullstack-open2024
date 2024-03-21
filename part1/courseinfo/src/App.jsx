9eb3a41f98ba60d63b84371097f2e9b43e6a8c15
Thalita dos Reis <dosreistha@gmail.com>
2024-03-21 12:53:24 +0100

    Exercises 1.1 - 1.5

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,62 @@
+const Header = ({ course }) => {
+  console.log(course);
+  
+  return <h1>{course.name}</h1>;
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} <span>{exercises}</span>
+    </p>
+  );
+};
+
+const Content = ({ parts }) => { 
+  return (
+    <div>
+      <Part name={parts[0].name} exercises={parts[0].exercises} />
+      <Part name={parts[1].name} exercises={parts[1].exercises} />
+      <Part name={parts[2].name} exercises={parts[2].exercises} />
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  const totalExercises =
+    parts[0].exercises +
+    parts[1].exercises +
+    parts[2].exercises;
+
+  return <h1>{totalExercises}</h1>;
+};
+
+function App() {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+}
+
+export default App;
