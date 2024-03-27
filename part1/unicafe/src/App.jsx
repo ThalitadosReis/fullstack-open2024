bcdf205af7f7e1eeb8f454fe0aa349a0c64c6d2e
Thalita dos Reis <dosreistha@gmail.com>
2024-03-27 15:17:59 +0100

    Exercise 1.11 unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,9 +6,10 @@ const Button = ({ handleClick, text }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <p>
-      {text}: {value}
-    </p>
+    <tr>
+      <td>{text}</td>
+      <td> {value}</td>
+    </tr>
   );
 };
 
@@ -21,14 +22,16 @@ const Statistics = ({
   positivePercentage,
 }) => {
   return (
-    <div>
-      <StatisticLine text="Good" value={good} />
-      <StatisticLine text="Neutral" value={neutral} />
-      <StatisticLine text="Bad" value={bad} />
-      <StatisticLine text="Total" value={total} />
-      <StatisticLine text="Average" value={average} />
-      <StatisticLine text="Positive" value={`${positivePercentage}%`} />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text="Good" value={good} />
+        <StatisticLine text="Neutral" value={neutral} />
+        <StatisticLine text="Bad" value={bad} />
+        <StatisticLine text="Total" value={total} />
+        <StatisticLine text="Average" value={average} />
+        <StatisticLine text="Positive" value={`${positivePercentage}%`} />
+      </tbody>
+    </table>
   );
 };
 
